b3ed8008a9f153e5c7f9bf88ff8fe0311426fb47
MohammadHossein Jahanbakhsh <mammadjahanbakhsh@yahoo.com>
2023-01-26 22:39:10 +0330

    finish video5

--- server/db.js
+++ server/db.js
@@ -5,5 +5,3 @@ const db = new Database('./data');
 export const Company = db.table('companies');
 export const Job = db.table('jobs');
 export const User = db.table('users');
-
-console.log(Company)
--- server/resolvers.js
+++ server/resolvers.js
@@ -1,14 +1,10 @@
-
+import {Job} from './db.js'
 
 export const resolvers = {
      Query:{
-        jobs: async()=>[ //can be regular or async function.mostly async because resolver usually should talk to database somehow 
-         {
-            id:'id',
-            description:'null',
-            title:'no'
-         }
-        ]
+        jobs: async()=>{ //can be regular or async function.mostly async because resolver usually should talk to database somehow 
+          return Job.findAll()
+      }
      }
 
 
